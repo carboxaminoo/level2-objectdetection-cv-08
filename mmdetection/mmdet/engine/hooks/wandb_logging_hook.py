40f7f5b910a786cd11e4ef5bf569199683498a8c
Jouhy <ethda11@kookmin.ac.kr>
2024-01-06 17:22:26 +0900

    Feat : Add wandb logging method for training  - train loss  - train loss rpn cls, rpn bbox, cls, bbox  - train acc #7

--- mmdetection/mmdet/engine/hooks/wandb_logging_hook.py
+++ mmdetection/mmdet/engine/hooks/wandb_logging_hook.py
@@ -6,55 +6,61 @@ from mmengine.runner import Runner
 
 from mmdet.registry import HOOKS
 
+import wandb
+
+
+
 @HOOKS.register_module()
 class WandbLoggingHook(Hook):
     """
     Wandb logging hook.
 
-    This hook will regularly saveing the experimential values
-    on wandb during training and validationing.
+    This hook regularly logs experimental values in wandb during training and validation.
 
     Args:
-        train_loss (float) : model training에서 사용한 loss
+        train_loss (float) : model training에서 사용한 loss, train_loss = loss_rpn_cls + loss_rpn_bbox + loss_cls + loss_bbox
+        train_loss_rpn_cls (float) : model training에서 사용한 rpn cls loss
+        train_loss_rpn_bbox (float) : model training에서 사용한 rpn bbox loss
+        train_loss_cls (float) : model training에서 사용한 cls loss
+        train_loss_bbox (float) : model training에서 사용한 bbox loss
         train_acc (float) : model training에서 사용한 acc
-        train_mAP (float) : model training에서 사용한 mean average precision
-        train_iou_score (float) : interest of union과 ground true 사이의
-                                  차이를 계산하여 출력
         
         val_loss (float) : model validation에서 사용한 loss
         val_acc (float) : model validation에서 사용한 acc
         val_mAP (float) : model validation에서 사용한 mean average precision
-        val_iou_score (float) : interest of union과 ground true 사이의
-                                차이를 계산하여 출력
 
     """
 
-    def __init__(
-        self,
-        train_loss: Optional[float] = None,
-        train_acc: Optional[float] = None,
-        train_mAP: Optional[float] = None,
-        train_iou_score: Optional[float] = None,
-        val_loss: Optional[float] = None,
-        val_acc: Optional[float] = None,
-        val_mAP: Optional[float] = None,
-        val_iou_score: Optional[float] = None,
-    ):
-        self.train_loss = 0.0
-        self.train_acc = 0.0
-        self.train_mAP = 0.0
-        self.train_iou_score = 0.0
+    def __init__(self):
         self.val_loss = 0.0
         self.val_acc = 0.0
         self.val_mAP = 0.0
-        self.val_iou_score = 0.0
         
     def after_train_iter(self, 
                          runner: Runner, 
                          batch_idx: int,
                          data_batch: dict,
                          outputs: Optional[dict] = None) -> None:
-        pass
+        """
+        Train에서 나온 loss, loss_rpn_cls, loss_rpn_bbox, loss_cls, loss_bbox, acc를 iter마다 wandb에 logging
+
+        Args:
+            outputs:
+                loss
+                loss_rpn_cls
+                loss_rpn_bbox
+                loss_cls
+                loss_bbox
+                acc            
+        """
+        wandb.log({
+            "train_loss": outputs['loss'],
+            "train_loss_rpn_cls": outputs['loss_rpn_cls'],
+            "train_loss_rpn_bbox": outputs['loss_rpn_bbox'],
+            "train_loss_cls": outputs['loss_cls'],
+            "train_loss_bbox": outputs['loss_bbox'],
+            "train_acc": outputs['acc']
+        })
 
     def before_val(self, runner: Runner):
         pass
