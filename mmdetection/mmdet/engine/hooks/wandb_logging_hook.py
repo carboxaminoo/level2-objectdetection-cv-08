556644a7c58802c3a3e87bb49a689841ece3d742
Jouhy <ethda11@kookmin.ac.kr>
2024-01-07 06:55:57 +0900

    Feat : Add mAP for training - training 중 pred_instances 받아오기 - METRICS에 eval_map 등록 - eval_map으로 mean_ap 받아오기(보완사항 : 0으로 나옴) #7

--- mmdetection/mmdet/engine/hooks/wandb_logging_hook.py
+++ mmdetection/mmdet/engine/hooks/wandb_logging_hook.py
@@ -1,11 +1,12 @@
 from typing import Optional, Dict
 
-import torch
+import numpy as np
 from mmengine.hooks import Hook
 from mmengine.runner import Runner
 
 from mmdet.registry import HOOKS
 from mmdet.registry import MODELS
+from mmdet.registry import METRICS
 
 import wandb
 
@@ -33,9 +34,11 @@ class WandbLoggingHook(Hook):
     """
 
     def __init__(self):
+        self.map = 0.0
         self.val_loss = 0.0
         self.val_acc = 0.0
         self.val_mAP = 0.0
+        self.mAP_fn = METRICS.module_dict['eval_map']
         
     def after_train_iter(self, 
                          runner: Runner, 
@@ -52,8 +55,51 @@ class WandbLoggingHook(Hook):
                 loss_rpn_bbox
                 loss_cls
                 loss_bbox
-                acc            
+                acc
+            pre_outs:
+                pred_instances:
+                    labels
+                    scores
+                    bboxes
+                gt_instances:
+                    labels
+                    bboxes
+            annotations(list[dict]): [img1_ann_dict, img2_ann_dict, …]
+                'bboxes': numpy array of shape (n, 4)
+                'labels': numpy array of shape (n, )
+            results(list[list]): [[cls1_det, cls2_det, …], …]
+                cls1_det: numpy array of shape (n, cls, 4) 
+                cls2_det: numpy array of shape (n, cls, 4)
+                …
+          
         """
+        # pred_instances를 가져올 수 있음
+        pre_outs = runner.model.val_step(data_batch)
+        pred_labels = []
+        pred_bboxes = []
+        annotations = []
+        det_results = []
+
+        for i in range(len(pre_outs)):
+            annotations.append({
+                'bboxes': pre_outs[i].gt_instances.bboxes.cpu().numpy(),
+                'labels': pre_outs[i].gt_instances.labels.cpu().numpy()
+            })
+            pred_bboxes = pre_outs[i].pred_instances.bboxes.cpu().detach().numpy()
+            pred_scores = pre_outs[i].pred_instances.scores.cpu().detach().numpy()
+            pred_labels = pre_outs[i].pred_instances.labels.cpu().numpy()
+            dets = []
+            for label in range(10):
+                index = np.where(pred_labels == label)[0]
+                pred_bbox_scores = np.hstack(
+                    [pred_bboxes[index], pred_scores[index].reshape((-1, 1))])
+                dets.append(pred_bbox_scores)
+            
+            det_results.append(dets)
+        ## 확인해야할 부분, recall, ap, mean_ap 값이 0으로 나옴
+        mean_ap, eval_results = self.mAP_fn(det_results, annotations)
+        self.map += mean_ap
+
         wandb.log({
             "train_loss": outputs['loss'],
             "train_loss_rpn_cls": outputs['loss_rpn_cls'],
@@ -103,7 +149,14 @@ class WandbLoggingHook(Hook):
         else:
             raise NotImplementedError
         cls_loss_function = MODELS.module_dict[cls_loss_function]
+        cls_loss_function = cls_loss_function()
         bbox_loss_function = MODELS.module_dict[bbox_loss_function]
+        bbox_loss_function = bbox_loss_function()
+
+
+        # loss값을 가져오려했는데, train_step에 loss.backward과정이 있어서
+        # RuntimeError: element 0 of tensors does not require grad and does not have a grad_fn 발생
+        # loss_outs = runner.model.train_step(data_batch, optim_wrapper=runner.optim_wrapper)
 
         # 근데 생각해보니까 cls loss는 model output logits으로 구해야하는데, pred_labels로 구하고 있음
         # 근데 model output logits을 여기서 찾을 수가 없음
@@ -111,17 +164,18 @@ class WandbLoggingHook(Hook):
         # 어떻게 할지 생각 해야할 듯...
         # val_loss_bbox = bbox_loss_function(pred_labels, gt_labels)
 
-        # 
-        val_loss_bbox = bbox_loss_function(pred_bboxes, gt_bboxes)
+        # pred_bboxes와 gt_bboxes의 사이즈가 안 맞음
+        # val_loss_bbox = bbox_loss_function(pred_bboxes, gt_bboxes)
 
         # mAP
+        # self.mAP_fn
 
         # cls acc
 
-        wandb.log({
-            "val_loss_bbox": val_loss_bbox,
-            # "val_acc": outputs['acc']
-        })
+        # wandb.log({
+        #     "val_loss_bbox": val_loss_bbox,
+        #     # "val_acc": outputs['acc']
+        # })
 
 
     def after_val_epoch(self,
--- mmdetection/mmdet/evaluation/functional/mean_ap.py
+++ mmdetection/mmdet/evaluation/functional/mean_ap.py
@@ -9,6 +9,8 @@ from terminaltables import AsciiTable
 from .bbox_overlaps import bbox_overlaps
 from .class_names import get_classes
 
+from mmdet.registry import METRICS
+
 
 def average_precision(recalls, precisions, mode='area'):
     """Calculate average precision (for single or multiple scales).
@@ -521,7 +523,7 @@ def get_cls_group_ofs(annotations, class_id):
 
     return gt_group_ofs
 
-
+@METRICS.register_module()
 def eval_map(det_results,
              annotations,
              scale_ranges=None,
